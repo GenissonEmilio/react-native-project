b69734cb0222a37baa715026553321fe8b8dfa3e
Genisson Emilio <esgenisson@gmail.com>
2025-06-25 20:40:29 -0300

    learning style

--- myApp/app/index.tsx
+++ myApp/app/index.tsx
@@ -1,9 +1,37 @@
-import { View, Text } from 'react-native';
+import { JSX } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Logo from '../assets/icon.png';
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Página Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>Construindo a página Home</Text>
+
+      <Text style={{ marginTop: 10}}>
+        Esta é a página inicial do aplicativo
+      </Text>
+      
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 12,
+  },
+  img: {
+    marginVertical: 20,
+    width: 50,
+    height: 50,
+    backgroundColor: '#000000',
+  },
+});
